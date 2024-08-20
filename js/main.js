3e05cf5493010519b7e360161b66b962732230cc
Amr maher <amrr.maherr24@gmail.com>
2024-08-21 02:28:49 +0300

    Add initial setup for form validation
    - Defined variables for subscription checkbox and payment method radio buttons
    - Added a reference to the submit button

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let subscribeCheckbox = document.getElementById('subscribeCheckbox');
+let visaRadio = document.getElementById('visaRadio');
+let masterCardRadio = document.getElementById("masterCardRadio");
+let paypalRadio = document.getElementById("paypalRadio");
+let btn = document.querySelector(".btn");
